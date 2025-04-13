a94f6227aff719a87a2a80169facc6628a82e9cc
AmirSina Mandegari <mandegari.amirsina@gmail.com>
2025-04-13 20:45:37 +0330

    Update base.py

--- django/core/files/base.py
+++ django/core/files/base.py
@@ -159,3 +159,4 @@ def endswith_lf(line):
 def equals_lf(line):
     """Return True if line (a text or bytestring) equals '\n'."""
     return line == ("\n" if isinstance(line, str) else b"\n")
+#test comment
